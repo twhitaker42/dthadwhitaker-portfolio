66d8afa0f01a91ed232898c6050e641ba3424056
D. Thad Whitaker <thad@Ds-MacBook-Air.local>
2025-07-22 10:40:57 -0600

    Updated Product cards

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,9 +6,6 @@ function HomePage() {
             <h1>Howdy! My name is Thad!</h1>
             <img src={profilePic} alt="Logo" id="profilePic"/>
             <h3>Product manager, fledgling software <br></br>developer*, and life-long learner</h3>
-            <br></br>
-            <br></br>
-            <br></br>
             <h5>This webpage is a work in progress. I'll be adding my experiences, products, and hobbies here shortly. *Yes, I'm making this site myself.</h5>
         </>
     );
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -5,16 +5,18 @@ function ProductPage() {
     return (
         <>
             <h1>Products</h1>
-            {ProductData.map((products, index) => (
-                <ProductCard
-                    key = {index}
-                    name = {products.name}
-                    description = {products.description}
-                    tools = {products.tools}
-                    wins = {products.wins}
-                    slug = {products.slug}
-                />
-            ))}
+            <div className="product-grid">
+                {ProductData.map((products, index) => (
+                    <ProductCard
+                        key = {index}
+                        name = {products.name}
+                        description = {products.description}
+                        tools = {products.tools}
+                        wins = {products.wins}
+                        slug = {products.slug}
+                    />
+                ))}
+            </div>
         </>
     );
 }
